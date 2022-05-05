a2cd353bf65568756a3a37e893e2a39608fe0a7b
shyamalaG-shyam <shyamalagopal719@gmail.com>
2022-05-05 15:22:21 +0530

    mysql connection

--- server/app.js
+++ server/app.js
@@ -2,6 +2,18 @@ const http = require('http');
 const port=process.env.PORT || 1337
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
+
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "fullStack",
+    password: "root@123"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 res.setHeader('Content-Type', 'text/html');
 res.end('<h1>Hello World</h1>');
 });
