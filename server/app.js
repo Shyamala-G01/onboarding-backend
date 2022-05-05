a797d9325dc4cca9e80b600fbdadb0398fb2555e
shyamalaG-shyam <shyamalagopal719@gmail.com>
2022-05-05 16:43:23 +0530

    s

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = process.env.PORT || 1337;
 var mysql = require("mysql");
   var con = mysql.createConnection({
-    host: "onboarding-backend.southindia.cloudapp.azure.com",
+    host: "http://onboarding-backend.southindia.cloudapp.azure.com",
     port:3306,
     user: "fullStack",
     password: "root@123",
