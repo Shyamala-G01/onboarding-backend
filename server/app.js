5d0ee4d801ab3e6e257eeaa66da295ad411d48a7
shyamalaG-shyam <shyamalagopal719@gmail.com>
2022-05-05 16:23:27 +0530

    final

--- server/app.js
+++ server/app.js
@@ -1,22 +1,25 @@
-const http = require('http');
-const port=process.env.PORT || 1337
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-
-var mysql = require('mysql');
-var con = mysql.createConnection({
+const http = require("http");
+const port = process.env.PORT || 1337;
+var mysql = require("mysql");
+  var con = mysql.createConnection({
     host: "onboarding-backend.southindia.cloudapp.azure.com:3306",
     user: "fullStack",
-    password: "root@123"
+    password: "root@123",
   });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
+
+  con.connect(function (err) {
+   
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected!");
+    }
   });
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Hello World</h1>");
 });
-server.listen(port,() => {
-console.log(`Server running at port `+port);
+server.listen(port, () => {
+  console.log(`Server running at port ` + port);
 });
