f59b0c97616667e328b55085869fc7fe17515ccd
shyamalaG-shyam <shyamalagopal719@gmail.com>
2022-05-06 13:34:07 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = process.env.PORT || 1337;
 var mysql = require("mysql");
   var con = mysql.createConnection({
-    host: "onboarding-backend.southindia.cloudapp.azure.com:3306",
+    host: "onboarding-backend.southindia.cloudapp.azure.com",
     user: "root",
     password: "",
     database:"ON_BOARDING"
