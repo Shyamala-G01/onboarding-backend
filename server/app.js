dc159e4568d81958b1bcfbd0e76aa9480752c151
shyamalaG-shyam <shyamalagopal719@gmail.com>
2022-05-09 15:39:51 +0530

    s

--- server/app.js
+++ server/app.js
@@ -1,21 +1,21 @@
 const http = require("http");
 const port = process.env.PORT || 1337;
-const app={}
-var mysql = require("mysql");
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "fullStack",
-    password: "root@123",
-    database:"ON_BOARDING"
-  });
 
-  con.connect(function(err) {
+var mysql = require("mysql");
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "fullStack",
+  password: "root@123",
+  database: "ON_BOARDING",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  con.query("SELECT * FROM roles", function (err, result, fields) {
     if (err) throw err;
-    con.query("SELECT * FROM roles", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
+    console.log(result);
   });
+});
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
