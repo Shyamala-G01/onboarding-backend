1086c1ec38276cbfe4369e51f6efaf31cb21a664
subrathKumar <subrathkumar.patra@diggibyte.com>
2022-05-09 17:58:51 +0530

    types rectification

--- server/model/admin.model.js
+++ server/model/admin.model.js
@@ -1,38 +1,41 @@
 const { Sequelize, DataTypes } = require("sequelize/types");
 
-
-// Define the  admin schema 
-module.exports = ( sequelize, DataTypes ) =>{
-    const Admin = sequelize.define("admins",{
-        id : {
-            type : DataTypes.String,
-            primaryKey : true
-        },
-        name : {
-            type : DataTypes.String
-        },
-        email : {
-            type : DataTypes.String
-        },
-        phone_number :{
-            type : DataTypes.String
-        },
-        password : {
-            type : DataTypes.String
-        },
-        designation:{
-            type : DataTypes.String
-        },
-        created_at:{
-            type : Date()
-        },
-        created_by_admin:{
-            type : DataTypes.String
-        }
-    },{
-        freezeTableName : true,
-        timestamps : false,
-        underscored : true
-    })
-    return Admin
-}
+// Define the  admin schema
+module.exports = (sequelize, DataTypes) => {
+  const Admin = sequelize.define(
+    "admins",
+    {
+      id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      phone_number: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      designation: {
+        type: DataTypes.STRING,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+      },
+      created_by_admin: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      freezeTableName: true,
+      timestamps: false,
+      underscored: true,
+    }
+  );
+  return Admin;
+};
