8dc8805ae4ca332a865f49ad4017c915ae3dcb8a
shyamalaG-shyam <shyamalagopal719@gmail.com>
2022-05-10 11:05:38 +0530

    adding env data for port checking

--- server/app.js
+++ server/app.js
@@ -20,6 +20,6 @@ app.get("/", (req, res) => {
 app.use("/api/admin", Router);
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server running at port ` + PORT);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at port ` + process.env.PORT);
 });
