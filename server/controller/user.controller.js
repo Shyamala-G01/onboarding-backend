f2d57bc41d250bd4290c67e1cfe2fafb5e268d18
subrathKumar <subrathkumar.patra@diggibyte.com>
2022-05-12 17:05:05 +0530

    address controller

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -3,42 +3,22 @@ const db = require("../model");
 const personalInfo = db.personalInfo;
 const address = db.address;
 
-// contoller for adding admin
 const addPersonalInfo = async (req, res) => {
-  // const personalData = await personalInfo.create(info);
   console.log(req.body);
-  let personData = {
-    fk_person_users_id: req.body.fk_person_users_id,
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    dob: req.body.dob,
-    gender: req.body.gender,
-    mobile_number: req.body.mobile_number,
-    alternate_number: req.body.alternate_number,
-    personal_email: req.body.personal_email,
-    photo: req.body.photo,
-    created_at: req.body.created_at,
-    updated_at: req.body.updated_at,
-    updated_by: req.body.updated_by,
-  };
-  // const userData = await personalInfo.create(personData);
-  // const userData = await sequelize.query(
-  //   `INSERT INTO personal_info(first_name,last_name,dob,gender,mobile_number,alternate_number,personal_email,photo,created_at,updated_at,updated_by,fk_person_users_id) VALUES(${personData.first_name},${personData.last_name},${personData.dob},${personData.gender},${personData.mobile_number},${personData.alternate_number},${personData.personal_email},${personData.photo},${personData.created_at},${personData.updated_at},${personData.updated_by},${personData.fk_person_users_id})`
-  // );
   const userData = await personalInfo.create(
     {
-      first_name: personData.first_name,
-      last_name: personData.last_name,
-      dob: personData.dob,
-      gender: personData.gender,
-      mobile_number: personData.mobile_number,
-      alternate_number: personData.alternate_number,
-      personal_email: personData.personal_email,
-      photo: personData.photo,
-      created_at: personData.created_at,
-      updated_at: personData.updated_at,
-      updated_by: personData.updated_by,
-      fk_person_users_id: personData.fk_person_users_id,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      dob: req.body.dob,
+      gender: req.body.gender,
+      mobile_number: req.body.mobile_number,
+      alternate_number: req.body.alternate_number,
+      personal_email: req.body.personal_email,
+      photo: req.body.photo,
+      created_at: req.body.created_at,
+      updated_at: req.body.updated_at,
+      updated_by: req.body.updated_by,
+      fk_person_users_id: req.body.fk_person_users_id,
     },
     {
       fields: [
@@ -64,12 +44,32 @@ const addPersonalInfo = async (req, res) => {
   }
 };
 
-const getAdmin = async (req, res) => {
-  let email = req.body.email;
-  let users = await admin.findOne({ where: { email: email } });
-  res.send(users);
-};
 
+// Address
+const addAddress = async (req, res) => {
+  console.log(req.body);
+  const info = {
+    type : req.body.type,
+    house_no : req.body.house_no,
+    street: req.body.street,
+    locality : req.body.locality,
+    city : req.body.city,
+    state : req.body.state,
+    pincode : req.body.pincode,
+    country : req.body.country,
+    created_at : req.body.created_at,
+    updated_at : req.body.updated_at,
+    updated_by : req.body.updated_by,
+    fk_address_users_id : req.body.fk_address_users_id,
+  };
+  const userData = await address.create(info)
+  if (userData) {
+    res.status(200).send({ message: "Successful" });
+  } else {
+    res.status(400).send({ message: "Unsuccessful" });
+  }
+};
 module.exports = {
   addPersonalInfo,
+  addAddress
 };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,6 +2,7 @@ const userController = require("../controller/user.controller");
 const Router = require("express").Router();
 
 Router.post("/addPersonalInfo", userController.addPersonalInfo);
+Router.post("/addAddress", userController.addAddress);
 
 
 module.exports = Router;
