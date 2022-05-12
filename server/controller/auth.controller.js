4180c53e6a4e22fab6afbcc9ed35b8c1e47fce01
shyamalaG-shyam <shyamalagopal719@gmail.com>
2022-05-12 15:25:02 +0530

    Update auth.controller.js

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -24,13 +24,13 @@ const getData = async (req, res) => {
           accessToken: token,
         });
       } else {
-        res.json({
+        res.send({
           status: 400,
           message: "invalid password",
         });
       }
     } else {
-      return res.json({
+      return res.send({
         status: 400,
         message: "user not found",
       });
