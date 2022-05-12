bd16c22a275c3f24a7192faeab31a131a277645a
subrathKumar <subrathkumar.patra@diggibyte.com>
2022-05-12 18:11:44 +0530

    mail sender configuration5

--- server/config/mail.config.js
+++ server/config/mail.config.js
@@ -1,7 +1,7 @@
 //to send mail v should install nodemailer
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-  service: "zoho",
+    host: 'smtppro.zoho.in',
   auth: {
     user: "test@diggibyte.com",
     pass: "Full$tack@!@#!!",
