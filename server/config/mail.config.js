710b0b18e7bf1d49794bc1255cd595a8fda8a085
shyamalaG-shyam <shyamalagopal719@gmail.com>
2022-05-26 17:56:09 +0530

    Update mail.config.js

--- server/config/mail.config.js
+++ server/config/mail.config.js
@@ -1,14 +1,27 @@
 //to send mail v should install nodemailer
+
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: "Outlook365",
+  host: "smtp.office365.com",
+
+  port: 587,
+
+  secure: false,
+requireTLS:false,
+  tls: {
+    rejectUnauthorized: false,
+  },
+
   auth: {
-    user: "subrathkumarpatra1996@gmail.com",
-    pass: "ewzoadaazyeocmst",
+    user: "test@diggibyte.com",
+    pass: "fqcmkhqwqwmdfgjm",
   },
 });
+
 const mailOptions = {
-  from: "subrathkumarpatra1996@gmail.com",
-  subject: "Welcome to Diggibyte Family",
+  from: "test@diggibyte.com",
+  subject: "Sending mail through node js on user added",
 };
+
 module.exports = { transporter, mailOptions };
