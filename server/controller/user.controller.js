6aa29ce19b2e6c963d2cf6ebdbf19776e0483f19
shyamalaG-shyam <shyamalagopal719@gmail.com>
2022-06-06 15:13:08 +0530

    Update user.controller.js

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -81,7 +81,7 @@ const updatePersonalInfo = async (req, res) => {
     mobile_number: req.body.mobile_number,
     alternate_number: req.body.alternate_number,
     personal_email: req.body.personal_email,
-    photo: req.body.photo,
+    photo: req.files.photo.name,
     created_at: req.body.created_at,
     updated_at: req.body.updated_at,
     updated_by: req.body.updated_by,
