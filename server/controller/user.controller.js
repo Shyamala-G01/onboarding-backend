bfe63fa67932bbcfba05adc4a4fec943ae6a0b35
shyamalaG-shyam <shyamalagopal719@gmail.com>
2022-06-06 18:26:45 +0530

    Update user.controller.js

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -20,64 +20,64 @@ const folderFunctions = require("../controller/fileHandler");
 
 //add data to personal info table
 const addPersonalInfo = async (req, res) => {
-  // const userData = await user.findOne({
-  //   where: { id: req.body.fk_person_users_id },
-  // });
+  const userData = await user.findOne({
+    where: { id: req.body.fk_person_users_id },
+  });
 
-  // const usercredential = await user.update(
-  //   { status: userData.status + 20 },
+  const usercredential = await user.update(
+    { status: userData.status + 20 },
 
-  //   { where: { id: req.body.fk_person_users_id } }
-  // );
+    { where: { id: req.body.fk_person_users_id } }
+  );
   console.log("s")
   console.log(req.files);
-  // let data = await personalInfo.findOne({
-  //   where: { fk_person_users_id: req.body.fk_person_users_id },
-  // });
-  // if (!data) {
-  //   const userData = await personalInfo.create(
-  //     {
-  //       first_name: req.body.first_name,
-  //       last_name: req.body.last_name,
-  //       dob: req.body.dob,
-  //       gender: req.body.gender,
-  //       mobile_number: req.body.mobile_number,
-  //       alternate_number: req.body.alternate_number,
-  //       personal_email: req.body.personal_email,
-  //       photo: req.files.photo.name,
-  //       father_name: req.body.father_name,
-  //       created_at: req.body.created_at,
-  //       updated_at: req.body.updated_at,
-  //       updated_by: req.body.updated_by,
-  //       fk_person_users_id: req.body.fk_person_users_id,
-  //     },
+  let data = await personalInfo.findOne({
+    where: { fk_person_users_id: req.body.fk_person_users_id },
+  });
+  if (!data) {
+    const userData = await personalInfo.create(
+      {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        dob: req.body.dob,
+        gender: req.body.gender,
+        mobile_number: req.body.mobile_number,
+        alternate_number: req.body.alternate_number,
+        personal_email: req.body.personal_email,
+        photo: req.files.photo.name,
+        father_name: req.body.father_name,
+        created_at: req.body.created_at,
+        updated_at: req.body.updated_at,
+        updated_by: req.body.updated_by,
+        fk_person_users_id: req.body.fk_person_users_id,
+      },
 
-  //     {
-  //       fields: [
-  //         "first_name",
-  //         "last_name",
-  //         "dob",
-  //         "gender",
-  //         "mobile_number",
-  //         "alternate_number",
-  //         "personal_email",
-  //         "photo",
-  //         "father_name",
-  //         "created_at",
-  //         "updated_at",
-  //         "updated_by",
-  //         "fk_person_users_id",
-  //       ],
-  //     }
-  //   );
-  // }
+      {
+        fields: [
+          "first_name",
+          "last_name",
+          "dob",
+          "gender",
+          "mobile_number",
+          "alternate_number",
+          "personal_email",
+          "photo",
+          "father_name",
+          "created_at",
+          "updated_at",
+          "updated_by",
+          "fk_person_users_id",
+        ],
+      }
+    );
+  }
 
-  // if (userData) {
-  //   folderFunctions.uploadfile(req.files,req.body.id)
-  //   res.status(200).send({ message: "Successful" });
-  // } else {
-  //   res.status(400).send({ message: "Unsuccessful" });
-  // }
+  if (userData) {
+    folderFunctions.uploadfile(req.files,req.body.id)
+    res.status(200).send({ message: "Successful" });
+  } else {
+    res.status(400).send({ message: "Unsuccessful" });
+  }
   res.send("s");
 };
 //update personal info table
