2f9456708ccce443da3175f4c8a312e8b93dcc5d
shyamalaG-shyam <shyamalagopal719@gmail.com>
2022-06-27 18:55:50 +0530

    Update user.controller.js

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -408,7 +408,6 @@ const getEducation = async (req, res) => {
 
 //update perticular/specific education i.e by id
 const updateEducation = async (req, res) => {
-  // console.log(req.files.marks_card)
   let id = req.params.id;
   let dat = await educationalInfo.findOne({
     where: { id: id },
@@ -436,7 +435,7 @@ const updateEducation = async (req, res) => {
         dat.provisional_marks_card,
         req.body.fk_education_users_id
       );
-      info.provisional_marks_card = req.files.provisionalCertificate.name;
+      info.provisional_marks_card = req.files.provisional_marks_card.name;
     }
     if (req.body.convocationCertificate != "") {
      
