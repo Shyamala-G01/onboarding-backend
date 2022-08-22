29a051a58ea128a74de8df1ef43f278ca24b6603
Chaya-PU <chaya.pu@diggibyte.com>
2022-08-22 12:53:13 +0530

    datatype is changed in id

--- server/model/citySatesCountry.model.js
+++ server/model/citySatesCountry.model.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
       "tb_country",
       {
         ID: {
-          type: DataTypes.INT,
+          type: DataTypes.INTEGER,
         
         },
        City: {
