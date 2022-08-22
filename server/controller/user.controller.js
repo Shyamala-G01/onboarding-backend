7cb6d282e3ac10927a9200b91e440ee36bc3f215
Chaya-PU <chaya.pu@diggibyte.com>
2022-08-22 14:45:57 +0530

    added console to test

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -242,7 +242,7 @@ const getStates = async(req,res)=>
 {
   let statesData = await statesname.findAll();
 }
-
+console.log(statesData.State);
 const updateEmployemnt = async (req, res) => {
   let ids = req.params.id;
   const dat = await employmentDetails.findOne({ where: { id: ids } });
