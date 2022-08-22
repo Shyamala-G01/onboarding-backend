f9c7415b6a62997b9a3879b4305e0c32fcb64801
Chaya-PU <chaya.pu@diggibyte.com>
2022-08-22 18:12:09 +0530

    added select query

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -240,7 +240,7 @@ const getEmployemnt = async (req, res) => {
 };
 const getStates = async(req,res)=>
 {
-  let statesData = await states.findAll({attributes:['State']});
+  let statesData = await states.findAll();
   console.log(statesData);
   res.send(statesData);
 }
