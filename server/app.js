0014a02bbe0041bc4ed8822e1f903697fa313d24
Mahananda Reddy <mahananda.reddy@diggibyte.com>
2022-09-08 13:15:50 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,8 +11,8 @@ const app = express();
 const cors = require("cors");
 var corsOption = {
     // origin:"http://localhost:4200",
-    // origin:"https://diggibyte.in",
-    origin:"http://diggibyte.azurewebsites.net"
+    origin:"https://diggibyte.in",
+    // origin:"http://diggibyte.azurewebsites.net"
 };
 app.use(fileuplod())
 
