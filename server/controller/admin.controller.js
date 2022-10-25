e6d4bb0356176e1350cab5c5a5b67b0d6400efd0
Chaya-PU <chaya.pu@diggibyte.com>
2022-10-25 16:24:27 +0530

    .

--- server/controller/admin.controller.js
+++ server/controller/admin.controller.js
@@ -1,6 +1,6 @@
 const sq = require("sequelize");
 const db = require("../model");
-const hbs = require("nodemailer-express-handlebars");
+
 const user = db.user;
 const admin = db.admin;
 //get all models of a user
@@ -100,10 +100,7 @@ const addEmployee = async (req, res) => {
     info.password = hashSync(pass, salt);
     const userData = await user.create(info);
     if (userData) {
-      transporter.use('compile',hbs({
-        viewEngine:'express-handlebars',
-        viewPath:'./views/'
-      }))
+     
       // sending mail after registration
       //to send mail on adding user
       mailOptions.to = `${info.email}`;
@@ -113,7 +110,7 @@ const addEmployee = async (req, res) => {
     //     filename: "emailtemplate.png",
     //     cid: "emailtemplate.png" + "@"
     //  }]);
-      (mailOptions.template='template');
+      (mailOptions.image='./views/images/emailtemplate.png');
 
         (mailOptions.text = ` 
 
